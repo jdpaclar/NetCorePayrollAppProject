b6a7f95eff00418d93b1ab49cbc6a0657d9d39c8
Jesse Paclar <jpaclardev@gmail.com>
2017-07-06 14:02:50 +1200

    Added a modal click function Angular

--- Company.Website.Payroll/wwwroot/js/App/Payroll/payrollCtrl.js
+++ Company.Website.Payroll/wwwroot/js/App/Payroll/payrollCtrl.js
@@ -3,15 +3,37 @@
 
     angular
         .module("payrollApp")
-        .controller("payrollCtrl", ["payrollResource", payrollCtrl]);
+        .controller("payrollCtrl", ["payrollResource", "$uibModal", payrollCtrl]);
 
 
-    function payrollCtrl(payrollResource) {
+    function payrollCtrl(payrollResource, $uibModal) {
         var vm = this;
 
         vm.calculatePayroll = function () {
             vm.loader = true;
-
+            
+            var calculatedPayroll = null;
+            
+            payrollResource.query({
+                
+            }, function (data) {
+                calculatedPayroll = data;
+                vm.loader = false;
+            });
+            
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: '/js/App/Payroll/calculatedPayrollModal.html',
+                controller: 'calculatedPayrollModalCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    detailItems: function() {
+                        return calculatedPayroll;
+                    }
+                }
+            });
             
         }
     }

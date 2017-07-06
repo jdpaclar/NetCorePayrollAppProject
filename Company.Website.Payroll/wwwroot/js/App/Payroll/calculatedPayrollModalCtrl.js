3c698d950810ebbf9df45bdf76ea4dc64013c906
Jesse Paclar <jpaclardev@gmail.com>
2017-07-06 14:06:56 +1200

    Added Modal Bindings

--- Company.Website.Payroll/wwwroot/js/App/Payroll/calculatedPayrollModalCtrl.js
+++ Company.Website.Payroll/wwwroot/js/App/Payroll/calculatedPayrollModalCtrl.js
@@ -8,6 +8,13 @@
     function calculatedPayrollModalCtrl($uibModalInstance, calculatedItemResult) {
         var vm = this;
 
+        vm.FullName = calculatedItemResult.fullName;
+        vm.PayPeriod = calculatedItemResult.payPeriod;
+        vm.GrossIncome = calculatedItemResult.grossIncome;
+        vm.IncomeTax = calculatedItemResult.incomeTax;
+        vm.NetIncome = calculatedItemResult.netIncome;
+        vm.Super = calculatedItemResult.super;
+        
         vm.close = function () {
             $uibModalInstance.dismiss('cancel');
         }
